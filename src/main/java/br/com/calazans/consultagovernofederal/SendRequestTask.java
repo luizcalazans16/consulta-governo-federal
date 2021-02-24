f52c449ad765bdd746fb43bb2800b58429661eaf
Luiz Calazans <luiz.calazans@viaflow.com.br>
2021-02-24 12:53:54 -0300

    Add task

---
+++ src/main/java/br/com/calazans/consultagovernofederal/SendRequestTask.java
@@ -0,0 +1,26 @@
+package br.com.calazans.consultagovernofederal;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.Callable;
+
+public class SendRequestTask implements Callable<HttpResponse<String>> {
+
+    private HttpClient httpClient;
+
+    private HttpRequest httpRequest;
+
+    public SendRequestTask(HttpClient client, HttpRequest request) {
+        this.httpClient = client;
+        this.httpRequest = request;
+    }
+
+    @Override
+    public HttpResponse<String> call() throws Exception {
+        return httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+    }
+
+
+}
--- src/main/java/br/com/calazans/consultagovernofederal/service/impl/AuxilioEmergencialServiceImpl.java
+++ src/main/java/br/com/calazans/consultagovernofederal/service/impl/AuxilioEmergencialServiceImpl.java
@@ -1,5 +1,6 @@
 package br.com.calazans.consultagovernofederal.service.impl;
 
+import br.com.calazans.consultagovernofederal.SendRequestTask;
 import br.com.calazans.consultagovernofederal.constants.Constants;
 import br.com.calazans.consultagovernofederal.dto.AuxilioEmergencialDto;
 import br.com.calazans.consultagovernofederal.dto.BeneficiarioDto;
@@ -7,21 +8,24 @@ import br.com.calazans.consultagovernofederal.dto.MunicipioDto;
 import br.com.calazans.consultagovernofederal.dto.response.GovernoFederalResponseDto;
 import br.com.calazans.consultagovernofederal.mapper.AuxilioEmergencialMapper;
 import br.com.calazans.consultagovernofederal.service.AuxilioEmergencialService;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Flow;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Service
+@Slf4j
 public class AuxilioEmergencialServiceImpl implements AuxilioEmergencialService {
 
     @Override
@@ -37,38 +41,50 @@ public class AuxilioEmergencialServiceImpl implements AuxilioEmergencialService 
                 .GET()
                 .build();
 
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            JSONArray jsonArray = new JSONArray(response.body());
+        HttpResponse<String> httpResponse = invokeCallable(new SendRequestTask(client, request));
+        JSONArray jsonArray = new JSONArray(httpResponse.body());
 
-            JSONObject jsonObject = null;
-            for (int i = 0; jsonArray.length() > i; i++) {
-                jsonObject = jsonArray.getJSONObject(i);
-                auxilioEmergencialList.add(AuxilioEmergencialDto.builder()
-                        .id(jsonObject.getInt("id"))
-                        .beneficiario(BeneficiarioDto.builder()
-                                .cpfFormatado(jsonObject.getJSONObject("beneficiario").getString("cpfFormatado"))
-                                .nome(jsonObject.getJSONObject("beneficiario").getString("nome"))
-                                .build())
-                        .municipioDto(MunicipioDto.builder()
-                                .nomeIBGE(jsonObject.getJSONObject("municipio").getString("nomeIBGE"))
-                                .build())
-                        .mesDisponibilizacao(jsonObject.getString("mesDisponibilizacao"))
-                        .valor(jsonObject.getDouble("valor"))
-                        .build());
-            }
-            for (AuxilioEmergencialDto auxilioEmergencialDto : auxilioEmergencialList) {
-                GovernoFederalResponseDto map = AuxilioEmergencialMapper.map(auxilioEmergencialDto);
-                governoFederalResponseList.add(map);
-            }
-            
-            return governoFederalResponseList;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        JSONObject jsonObject;
+        for (int i = 0; jsonArray.length() > i; i++) {
+            jsonObject = jsonArray.getJSONObject(i);
+            auxilioEmergencialList.add(AuxilioEmergencialDto.builder()
+                    .id(jsonObject.getInt("id"))
+                    .beneficiario(BeneficiarioDto.builder()
+                            .cpfFormatado(jsonObject.getJSONObject("beneficiario").getString("cpfFormatado"))
+                            .nome(jsonObject.getJSONObject("beneficiario").getString("nome"))
+                            .build())
+                    .municipioDto(MunicipioDto.builder()
+                            .nomeIBGE(jsonObject.getJSONObject("municipio").getString("nomeIBGE"))
+                            .build())
+                    .mesDisponibilizacao(jsonObject.getString("mesDisponibilizacao"))
+                    .valor(jsonObject.getDouble("valor"))
+                    .build());
         }
+        for (AuxilioEmergencialDto auxilioEmergencialDto : auxilioEmergencialList) {
+            GovernoFederalResponseDto map = AuxilioEmergencialMapper.map(auxilioEmergencialDto);
+            governoFederalResponseList.add(map);
+        }
+        log.info("Registros de auxílio emergencial encontrados: [{}]", governoFederalResponseList);
         return governoFederalResponseList;
     }
+
+    private HttpResponse<String> invokeCallable(SendRequestTask task) {
+        System.out.println("Abrindo thread...");
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        System.out.println("Enviando requisição para o serviço do governo federal...");
+        Future<HttpResponse<String>> futureCall = executorService.submit(task);
+        HttpResponse<String> result = null;
+
+        try {
+            result = futureCall.get();
+            System.out.println("Shutting down...");
+            executorService.shutdown();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
 }
